3dffcf59f788559759d3d1ed70a1c6f9e8e82e5f
sadrda <kofler_a@yahoo.de>
2021-12-06 18:01:12 +0100

    day6

---
+++ 2021/a6.scala
@@ -0,0 +1,26 @@
+import java.util.ArrayList
+import scala.collection.mutable.ArrayBuffer
+
+def repopulate(fishCount: Array[Long]) = {
+  val newFishCount = Array.fill(9)(0L)
+  for(n <- 0 until 9) {
+    if(n == 8) newFishCount(8) = fishCount(0)
+    else newFishCount(n) = fishCount(n+1)
+  }
+  newFishCount(6) += fishCount(0)
+  newFishCount
+}
+
+def main(args: Array[String]) = {
+  var fishes = scala.io.Source.fromFile("a6.txt").mkString.split(",").map(_.toInt)
+  
+  var fishCount = Array.fill(9)(0L)
+  for(fish <- fishes) {
+    fishCount(fish) += 1
+  }
+
+  for(n <- 0 until 256) {
+    fishCount = repopulate(fishCount)
+  }
+  println(fishCount.sum)
+}
