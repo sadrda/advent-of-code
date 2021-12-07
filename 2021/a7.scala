c798433159e4e481a5228bfc452c68e91acb6278
sadrda <kofler_a@yahoo.de>
2021-12-07 08:42:07 +0100

    day7

---
+++ 2021/a7.scala
@@ -0,0 +1,21 @@
+def factorialSum(n: Int): Int = n match {
+    case 0 => 0
+    case _ => n + factorialSum(n-1)
+}
+
+def main(args: Array[String]) = {
+  val input = scala.io.Source.fromFile("a7.txt").mkString.split(",").map(_.toInt)
+
+  val fuelCosts = Array.fill(input.max)(0)
+  for(i <- 0 until input.max) {
+    var sum = 0
+    for(j <- 0 until input.length) {
+      val curr = input(j)
+      val biggerNum = i.max(curr)
+      val smallerNum = i.min(curr)
+      sum += factorialSum(biggerNum - smallerNum)
+    }
+    fuelCosts(i) = sum
+  }
+  println(fuelCosts.min)
+}
